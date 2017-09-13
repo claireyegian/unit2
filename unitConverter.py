e5963b826507809f7295e24200fdb10cfe75b95a
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-13 13:59:26 -0400

    Updated from Brython Server: 9/13/2017 1:59:25 PM

--- unitConverter.py
+++ unitConverter.py
@@ -1,1 +1,6 @@
+#Claire Yegian
+#9/13/17
+#unitConverter.py - converts kilometers to miles, kilograms to pounds, liters to gallons, and Celsius to Fahrenheit
+
+num = int(input('Enter number 1 for kilometers to miles, 2 for kilograms to pounds, 3 for liters to gallons, 4 for and Celsius to Fahrenheit: '))
 

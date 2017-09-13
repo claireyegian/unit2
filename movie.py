0a36e28e15391c79bd4ac45f0fccfd65861813a3
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-13 13:52:20 -0400

    Updated from Brython Server: 9/13/2017 1:52:19 PM

--- movie.py
+++ movie.py
@@ -1,1 +1,15 @@
+#Claire Yegian
+#9/13/17
+#movie.py - how scandalous can you watch?
 
+age = int(input('Enter your age: '))
+if age>=18:
+    print('You can watch NC-17 rated movies')
+elif age>=17:
+    print('You can watch R-rated movies')
+elif age>=13:
+    print('You can watch PG-13 rated movies')
+elif age>=8:
+    print('You can watch PG-rated movies')
+else:
+    print('You can watch G-rated movies')

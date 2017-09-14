488deac308deaac00c0c26b0f2e5fb3698ec19b9
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-14 14:46:31 -0400

    Updated from Brython Server: 9/14/2017 2:46:31 PM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -1,1 +1,22 @@
+#Claire Yegian
+#9/14/17
+#fortuneTeller.py - tells user fortune
 
+color = input('Pick a color(red or blue): ')
+num = int(input('Pick a number 1-4: '))
+if color=='red' and num==1:
+    print('You will live as a hermit in a small mountain hermit community near the Himalayas')
+elif color=='red' and num==2:
+    print('You will purchase a cat and name it Bilbo, only to find that it has a severe case of asthma and is on the fast track to death')
+elif color=='red' and num==3:
+    print('You will develop a mental illness that causes an obsession with hair, to satisfy your needs, you will become a hairdresser and save every strand of hair from every client you ever serve')
+elif color=='red' and num==4:
+    print('You will have an obnoxious child and you will name him Clay. He will grow up to think he is cool, when in fact, he is not')
+elif color=='blue' and num==1:
+    print('Within the next five years, you will discover a keyhole in the earths crust, allowing humans to look into the center. By looking into the center, you will discover that the world as you know it is just this program, enlarged to incorporate human lives')
+elif color=='blue' and num==2:
+    print('You will become a fortune teller only to discover that your fortunes are way too elaborate and are cannot support your expensive lifestyle. You will quit your job and end up living in a spacious cardboard box in the suberbs of a small city in Connecticut')
+elif color=='blue' and num==3:
+    print('You will be mauled by a bear in the near future')
+elif color=='blue' and num==4:
+    print('You will be benched')

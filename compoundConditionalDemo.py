26893592fa53881be028c31886a2a47d5b6e7288
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-14 14:22:20 -0400

    Update compoundConditionalDemo.py

--- compoundConditionalDemo.py
+++ compoundConditionalDemo.py
@@ -1,1 +1,16 @@
+#Claire Yegian
+#9/14/17
+#compoundConditionalDemo.py - and/or
 
+num = float(input('Enter a number: '))
+
+if num>0 and num%7==0:
+    print('Your number is positive and divisible by seven')
+elif num>0:
+    print('Your number is positive and not divisible by seven :(')
+elif num<0 and num%7==0:
+    print('Your number is negative and divisible by seven')
+elif num<0:
+    print('Your number is negative and not divisible by seven :(')
+else:
+    print('Your number is zero')

e9717c840c3849124e6d1d155d8d32b4a7c495c1
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-14 14:55:10 -0400

    Updated from Brython Server: 9/14/2017 2:55:09 PM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,20 @@
+#Claire Yegian
+#9/14/17
+#insulter.py - insults user
 
+from random import randint
+
+name = input('Enter your name: ')
+number = randint(1,6)
+if number==1:
+    print('I would unplug your life support to charge my phone')
+elif number==2:
+    print('You bring everyone a lot of joy... when you leave the room')
+elif number==3:
+    print('I bet your brain feels as good as new, seeing as you never use it')
+elif number==4:
+    print('You look like something I would draw with my left hand')
+elif number==5:
+    print('If I had to bench anyone in the game of life, it would be you')
+elif number==6:
+    print('

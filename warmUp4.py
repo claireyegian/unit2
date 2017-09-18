165208479e7017ee73db9b73569ffe346d473bd3
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-18 14:22:30 -0400

    Updated from Brython Server: 9/18/2017 2:22:30 PM

--- warmUp4.py
+++ warmUp4.py
@@ -1,1 +1,9 @@
+#Claire Yegian
+#9/18/17
+#warmUp.py - find 7 or divisible by 7 game
 
+num = int(input('Enter a number: '))
+if '7' in str(num) or num%7==0:
+    print('Buzz')
+else:
+    print(num)

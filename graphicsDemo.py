804f4a4fb559a09f8b4715d8f22d90dbf4e38a99
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-22 14:33:12 -0400

    Update graphicsDemo.py

--- graphicsDemo.py
+++ graphicsDemo.py
@@ -14,19 +14,14 @@ blackOutline = LineStyle(1,black) #pixels, color
 redRectangle = RectangleAsset(200,100,blackOutline,red) #width, height, outline, fill
 blueCircle = CircleAsset(50,blackOutline,blue) #radius, outline, fill
 greenEllipse = EllipseAsset(100,50,blackOutline,green) #horizontal_radius, vertical_radius, outline, fill
+blackLine = LineAsset(60,150,blackOutline) #endpoint_x, endpoint_y, line style
+redTriangle = PolygonAsset([(0,0),(120,180),(60,300)],blackOutline,red) #endpoints, outline, fill
+text = TextAsset('Yegian', fill=blue, style='bold 40pt Times') #text, other options
 
 Sprite(redRectangle)
 Sprite(blueCircle,(50,50)) #Sprite(Circle, (point))
 Sprite(greenEllipse, (200,400))
+Sprite(blackLine)
+Sprite(redTriangle)
+Sprite(text,(300,200))
 App().run()
-
-
-
-
-
-
-
-
-
-
-

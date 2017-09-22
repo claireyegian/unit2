c6bb1026410d9548dd58908576519484bb1969cd
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-22 14:53:21 -0400

    Updated from Brython Server: 9/22/2017 2:53:22 PM

--- house.py
+++ house.py
@@ -14,9 +14,11 @@ Outline = LineStyle(1,black)
 
 greyRectangle = RectangleAsset(400,200,blackOutline,grey)
 greyTriangle = PolygonAsset([(300,200),(700,200),(500,100)],blackOutline,grey)
-door = RectangleAsset(65,100,Outline,darkGrey)
+door = RectangleAsset(65,100,blackOutline,darkGrey)
+window = RectangleAsset(60,60,blackOutline,blue)
 
 Sprite(greyRectangle,(300,200))
 Sprite(greyTriangle)
-Sprite(door,(475,300))
+Sprite(door,(460,300))
+Sprite(window,(370,250))
 App().run()

18c5d6f6b43785c02fecf702d62e9047a98a512a
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-22 14:51:29 -0400

    Updated from Brython Server: 9/22/2017 2:51:29 PM

--- house.py
+++ house.py
@@ -5,14 +5,18 @@
 from ggame import *
 
 grey = Color(0xc0c0c0,1)
+darkGrey = Color(0x606060,1)
 blue = Color(0xccffff,1)
 black = Color(0x000000,1)
 
 blackOutline = LineStyle(0,black)
+Outline = LineStyle(1,black)
 
 greyRectangle = RectangleAsset(400,200,blackOutline,grey)
 greyTriangle = PolygonAsset([(300,200),(700,200),(500,100)],blackOutline,grey)
+door = RectangleAsset(65,100,Outline,darkGrey)
 
 Sprite(greyRectangle,(300,200))
 Sprite(greyTriangle)
+Sprite(door,(450,300))
 App().run()

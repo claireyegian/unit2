0ad0cddf02e3c2e2cf34f3803501af894c29b018
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-09-22 14:45:46 -0400

    Updated from Brython Server: 9/22/2017 2:45:47 PM

--- house.py
+++ house.py
@@ -1,1 +1,16 @@
+#Claire Yegian
+#9/22/17
+#house.py - prints house image
 
+from ggame import *
+
+grey = Color(0xc0c0c0,1)
+blue = Color(0xccffff,1)
+black = Color(0x000000,1)
+
+blackOutline = LineStyle(0,black)
+
+greyRectangle = RectangleAsset(400,200,blackOutline,grey)
+
+Sprite(greyRectangle,(300,200))
+App().run()
